aca6decf80fbbcbf31fd4e5f32de265563df039d
manish-rocks <manish21.12.15@gmail.com>
2022-12-19 02:47:19 +0530

    added files for tests

---
+++ app/app/calc.py
@@ -0,0 +1,22 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """add x and y
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+    return: x+y
+    """
+    return x+y
+    
+def subtract(x,y):
+    """subtract x from y and return result.
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+    """
+    return abs(x-y)    
---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+"""
+sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """_summary_
+
+    Args:
+        SimpleTestCase (_type_): _description_
+    """
